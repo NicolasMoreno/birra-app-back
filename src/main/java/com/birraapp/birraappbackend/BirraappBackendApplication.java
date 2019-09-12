832ae6cf3e1e0237e582b659b4735dd696251c0f
nicolas.moreno <nicolas.moreno@ing.austral.edu.ar>
2019-09-12 19:09:44 -0300

    initial commit

---
+++ src/main/java/com/birraapp/birraappbackend/BirraappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.birraapp.birraappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirraappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirraappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birraapp/birraappbackend/BirraappBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.birraapp.birraappbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BirraappBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

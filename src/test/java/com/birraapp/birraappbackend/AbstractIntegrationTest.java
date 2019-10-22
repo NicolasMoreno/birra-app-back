d77db479c69547340d7aa4ded039b5cdb0a0cba4
nicolas.moreno <nicolas.moreno@ing.austral.edu.ar>
2019-10-21 23:44:39 -0300

    Finished docker-compose script.

--- src/test/java/com/birraapp/birraappbackend/AbstractIntegrationTest.java
+++ src/test/java/com/birraapp/birraappbackend/AbstractIntegrationTest.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {BirraappBackendApplication.class})
 public abstract class AbstractIntegrationTest {
